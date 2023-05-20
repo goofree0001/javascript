c6e3d2e3cc29e6af07b5bb9632d8f35c6e1b4b53
goofree0001 <85332508+goofree0001@users.noreply.github.com>
2023-05-20 13:13:18 +0900

    Add files via upload

--- extesion/open_html/content_script.js
+++ extesion/open_html/content_script.js
@@ -1,1 +1,14 @@
-window.open(chrome.runtime.getURL("alert.html"));
+var find_flag = false;
+chrome.tabs.query({ currentWindow: true }, function(tabs) {
+	for (var i = 0; i < tabs.length; i++) {
+		if (tabs[i].url == chrome.runtime.getURL("alert.html")) {
+			find_flag = true;
+			break;
+		}
+	}
+});
+
+if (find_flag == false) {
+	window.open(chrome.runtime.getURL("alert.html"));
+}
+
